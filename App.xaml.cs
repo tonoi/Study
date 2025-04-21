62775867398b49bdb0e8fb6d989fd53661e3eaae
Mitsuo-Koikawa <mkoikawa@tonoi.co.jp>
2025-04-21 11:51:38 +0900

    プロジェクト ファイルを追加します。

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Study
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
